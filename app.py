b50068a9b3622747fcb57d02e2c9b0005706a4f2
cooltoast <mpatel678@gmail.com>
2015-07-26 14:24:30 -0700

    add basic stuff

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template('main.html')
+
+@app.route("/hello")
+def hello():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.run()
