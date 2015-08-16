55fdcaac65c0e2a184c1fc99559bb8511a726a8f
Hurshal Patel <hurshal@memsql.com>
2015-08-15 23:28:03 -0700

    do the twilio

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-from flask import render_template
+import json
+from flask import Flask, render_template, request
+from twilio import twiml
+
 app = Flask(__name__)
 
+try:
+  AUTHORIZED_SENDERS = json.load(open("authorized_senders.json"))
+except Exception as e:
+  print(e)
+  print("NO SENDERS AUTHORIZED")
+  AUTHORIZED_SENDERS = []
+
+
 def render_page(name, *args, **kwargs):
-    kwargs['selected'] = name
-    return render_template('%s.html' % name, *args, **kwargs)
+  kwargs['selected'] = name
+  return render_template('%s.html' % name, *args, **kwargs)
 
 @app.route("/")
 def home():
@@ -25,6 +35,23 @@ def newsfeed():
 @app.route("/photos")
 def photos():
   return render_page('photos')
+
+@app.route("/twilio", methods=['GET', 'POST'])
+def twilio():
+  resp = twiml.Response()
+  sender = request.values.get('From', None)
+  body = request.values.get('Body', None)
+
+  if sender in AUTHORIZED_SENDERS:
+    if body is not None:
+      print("%s: %s" % (sender, body))
+      resp.message("test")
+    else:
+        resp.message("but no message")
+  else:
+    resp.message("no youre not authorized")
+
+  return str(resp)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
